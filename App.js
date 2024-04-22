02137ae46b505b340f5e63dbcb3a97bf21e5564a
hieu2k2bb <2024802010415@student.tdmu.edu.vn>
2024-04-22 13:49:34 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { PaperProvider } from "react-native-paper";
+import MyStack from "./routers/MyStack";
+import MyDrawer from "./routers/MyDrawer";
+import MyBottomTabNavigator from "./routers/MyBottomTabNavigator";
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <PaperProvider>
+        <MyStack/>
+      </PaperProvider>
+    </NavigationContainer>
+  );
+}
